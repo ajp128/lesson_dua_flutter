5cdc129c1b0449cf8bcc1a51539affa9b4e8b53d
ajp128 <alifhunter87@gmail.com>
2020-12-22 22:55:01 +0700

    latihan course 2 done

--- widget/lib/detail_screen.dart
+++ widget/lib/detail_screen.dart
@@ -31,9 +31,63 @@ class _DetailScreenState extends State<DetailScreen> {
                           spreadRadius: 2)
                     ],
                     borderRadius: BorderRadius.all(Radius.circular(5))),
-                height: 250,
                 width: 500,
-              ),
+                padding: const EdgeInsets.only(
+                    top: 10.0, left: 10.0, bottom: 20.0, right: 10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.network(
+                      'https://static.wikia.nocookie.net/kimetsu-no-yaiba/images/f/fd/Kyojuro_anime.png/revision/latest/scale-to-width-down/350?cb=20191117152140',
+                      width: 280,
+                      height: 400,
+                      fit: BoxFit.cover,
+                    ),
+                    Table(
+                      border: TableBorder.all(),
+                      children: [
+                        TableRow(children: [
+                          Column(children: [Text('Name')]),
+                          Column(children: [Text('Kyojuro Rengoku')])
+                        ]),
+                        TableRow(children: [
+                          Column(children: [Text('Kanji')]),
+                          Column(children: [Text('煉獄 杏寿郎')])
+                        ]),
+                        TableRow(children: [
+                          Column(children: [Text('Birth of Date')]),
+                          Column(children: [Text('May 10th')])
+                        ]),
+                        TableRow(children: [
+                          Column(children: [Text('Gender')]),
+                          Column(children: [Text('Male')])
+                        ]),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        RaisedButton(
+                          onPressed: () {},
+                          color: Colors.red[900],
+                          child: Text(
+                            'Save Me!',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        RaisedButton(
+                          onPressed: () {},
+                          color: Colors.red[900],
+                          child: Text(
+                            'Let Me GO!',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
--- widget/lib/home_screen.dart
+++ widget/lib/home_screen.dart
@@ -6,17 +6,37 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Widget'),
-        // centerTitle: true,
-        leading: Icon(Icons.home),
-      ),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Container(
-            color: Colors.red,
-            child: Row(
+        appBar: AppBar(
+          title: Text('alip ganteng'),
+          centerTitle: true, //bikin title ke tengah
+          leading: Icon(Icons.home), //bikin icon atau title di depan
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Image.network(
+                'https://cdn.idntimes.com/content-images/community/2018/11/tempat-mendirikan-tenda-1a46ade4a8c07e6367ed93d728bb0e4f.jpg',
+                height: 200,
+                width: 200,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Text(
+              'Unicorn Pixel',
+              style: TextStyle(color: Colors.amber),
+            ),
+            Container(
+              child: Row(
+                children: [
+                  Icon(Icons.directions),
+                  Icon(Icons.directions),
+                  Icon(Icons.directions),
+                  Icon(Icons.directions),
+                ],
+              ),
+            ),
+            Column(
               children: [
                 Icon(Icons.directions),
                 Icon(Icons.directions),
@@ -24,34 +44,19 @@ class HomeScreen extends StatelessWidget {
                 Icon(Icons.directions),
               ],
             ),
-          ),
-          Container(
-            color: Colors.blue,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(Icons.directions),
-                Icon(Icons.directions),
-                Icon(Icons.directions),
-                Icon(Icons.directions),
-              ],
+            RaisedButton(
+              onPressed: () {
+                //pindah page
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DetailScreen()));
+              },
+              child: Text(
+                'Tombol raised',
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.green,
             ),
-            //button
-          ),
-          RaisedButton(
-            onPressed: () {
-              //pindah page
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => DetailScreen()));
-            },
-            child: Text(
-              'Tombol raised',
-              style: TextStyle(color: Colors.white),
-            ),
-            color: Colors.green,
-          ),
-        ],
-      ),
-    );
+          ],
+        ));
   }
 }
